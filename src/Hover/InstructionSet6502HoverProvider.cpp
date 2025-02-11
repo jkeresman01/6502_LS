be5fa774ed7eb654c787f0237b45429721a8313c
josip <josipkeresman@gmail.com>
2025-02-11 11:51:54 +0100

    Implement intruction set hover provider

--- src/Hover/InstructionSet6502HoverProvider.cpp
+++ src/Hover/InstructionSet6502HoverProvider.cpp
@@ -1,17 +1,40 @@
-#pragma once
+#include "InstructionSet6502HoverProvider.h"
 
-#include <memory>
+#include "../Types/HoverItem.h"
+#include "../Types/Position.h"
 
-#include "IHoverProvider.h"
+#include "../Utils/DocumentUtil.h"
+#include "../Repo/InstructionSetRepoFactory.h"
 
 namespace ls6502
 {
-class InstructionSet6502HoverProvider : public IHoverProvider
-{
-  public:
-    HoverItem getHoverItems(const std::string &document, const Position &position) override;
 
-  private:
-    std::shared_ptr<IInstructionSetRepository> m_instructionSetRepository;
-};
+    InstructionSet6502HoverProvider::InstructionSet6502HoverProvider()
+        : m_instructionSetRepository(IInstructionSetRepoFactory::create())
+    {
+        m_instructionSet = m_instructionSetRepository->load();
+    }
+
+    HoverItem InstructionSet6502HoverProvider::getHoverItems(const std::string &document, const Position &position)
+    {
+        std::string instruction = DocumentUtil::extractPrefix(document, position);
+
+        LS_6502_DEBUG(STR("Hover items are requested for instruction: %s", instruction.c_str()));
+
+        InstructionSetMapT::iterator it = m_instructionSet.find(instruction);
+
+        if(it == m_instructionSet.end())
+        {
+            LS_6502_DEBUG(STR("Following instruction isn't supporeted: %s", instruction.c_str()));
+            return {};
+        }
+
+        Instruction instructionDetails = it->second;
+
+        HoverItem hoverItem;
+        hoverItem.text = instructionDetails.toString();
+
+        return hoverItem;
+    }
+
 } // namespace ls6502
--- src/Hover/InstructionSet6502HoverProvider.h
+++ src/Hover/InstructionSet6502HoverProvider.h
@@ -1,15 +1,26 @@
 #pragma once
 
-#include "../Types/Position.h"
-#include "IHoverProvider.h"
-
 #include <vector>
+#include <memory>
+
+#include "../Types/Position.h"
+#include "../Repo/IInstructionSetRepository.h"
+#include "IHoverProvider.h"
 
 namespace ls6502
 {
+typedef std::unordered_map<std::string, Instruction> InstructionSetMapT;
+
 class InstructionSet6502HoverProvider : public IHoverProvider
 {
   public:
+    InstructionSet6502HoverProvider();
+
     HoverItem getHoverItem(const std::string &document, const Position &position) override;
+
+  private:
+    std::shared_ptr<IInstructionSetRepository> m_instructionSetRepository;
+
+    InstructionSetMapT m_instructionSet;
 };
 } // namespace ls6502
