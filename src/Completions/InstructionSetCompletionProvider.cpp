608e571aafde6253bb8967100688936b80c18943
josip <josipkeresman@gmail.com>
2025-02-11 19:37:53 +0100

    Prepare some stuff for instruction set completion provider

--- src/Completions/InstructionSetCompletionProvider.cpp
+++ src/Completions/InstructionSetCompletionProvider.cpp
@@ -1,6 +1,9 @@
 #include "InstructionSetCompletionProvider.h"
 
 #include "../Repo/InstructionSetRepoFactory.h"
+#include "../Types/Position.h"
+#include "../Utils/DocumentUtil.h"
+#include "../Utils/Logger.h"
 
 namespace ls6502
 {
@@ -19,9 +22,22 @@ void InstructionSetCompletionProvider::loadInstructionSetTrie()
     }
 }
 
-std::vector<CompletionItem> InstructionSetCompletionProvider::getCompletions() override
+std::vector<CompletionItem> InstructionSetCompletionProvider::getCompletions(const std::string &document, const Positon &position) override
 {
-    // TODO
+    std::string instruction = DocumentUtil::extractPrefix(document, position);
+
+    InstructionSetMapT::iterator it = m_instructionSet.find(instruction);
+
+    if(it == m_instructionSet.end())
+    {
+        LS_6502_WARN(STR("%s isn't a vaid 6502 assembly instruction", instruction.c_str()));
+        return {};
+    }
+
+    Instruction instructionDetails = it->second;
+
+    //TODO finish creat comletion item, return vector
+
 }
 
 } // namespace ls6502
