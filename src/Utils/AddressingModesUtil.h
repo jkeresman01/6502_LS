1de79cf4c00db22ca17ead710bfd0ca30e84238e
josip <josipkeresman@gmail.com>
2025-02-24 17:20:47 +0100

    Document addressing mode util

--- src/Utils/AddressingModesUtil.h
+++ src/Utils/AddressingModesUtil.h
@@ -16,12 +16,48 @@ namespace ls6502
 typedef std::unordered_map<std::string, AddressingModes> StringAddressingModeMapT;
 typedef std::unordered_map<AddressingModes, std::string> AddressingModeStringMapT;
 
+//////////////////////////////////////////////////////////////
+///
+/// @class AddressingModeUtil
+///
+/// @brief A utility class that provides conversion
+///        functions between string representations and
+///        enumeration values of addressing modes.
+///
+//////////////////////////////////////////////////////////////
 class AddressingModeUtil
 {
   public:
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Deleted default constructor to prevent instantiation.
+    ///
+    //////////////////////////////////////////////////////////////
     AddressingModeUtil() = delete;
 
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Converts a string representation of an addressing mode
+    ///        to its corresponding enum value.
+    ///
+    /// @param addressingMode
+    ///
+    /// @return The corresponding AddressingModes enum value.
+    ///
+    //////////////////////////////////////////////////////////////
     static AddressingModes stringToAddressingMode(const std::string &addressingMode);
+
+    //////////////////////////////////////////////////////////////
+    ///
+    /// @brief Converts an addressing mode enum value to
+    ///        its string representation.
+    ///
+    /// @param addressingMode
+    ///
+    /// @return A C-string representing the addressing mode.
+    ///
+    //////////////////////////////////////////////////////////////
     static const char *addressingModeToString(const AddressingModes addressingMode);
 };
+
 } // namespace ls6502
