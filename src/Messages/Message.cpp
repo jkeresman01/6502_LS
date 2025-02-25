faa45aea4961a0209a4dbed6c3f4fac2cbb2391b
josip <josipkeresman@gmail.com>
2025-02-25 23:29:56 +0100

    Add impl file for Message.h

---
+++ src/Messages/Message.cpp
@@ -0,0 +1,22 @@
+#include "Message.h"
+
+namespace ls6502
+{
+Message::Message(const std::string &jsonRPC) : m_jsonRPC(jsonRPC) {}
+
+nlohmann::json Message::toJson() const
+{
+    return {{"jsonrpc", m_jsonRPC}};
+}
+
+std::ostream &operator<<(std::ostream &os, const Message &mesage)
+{
+    nlohmann::json jsonRPC = mesage.toJson();
+
+    os << "Content-Length: " << jsonRPC.dump().size() << "\r\n\r\n";
+    os << jsonRPC.dump() << std::endl;
+
+    return os;
+}
+
+} // namespace ls6502
