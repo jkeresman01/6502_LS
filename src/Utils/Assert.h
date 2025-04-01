a87e564e64f5eae03e1a0466c7dd17e40710cd6c
josip <josipkeresman@gmail.com>
2025-04-01 10:39:51 +0200

    Define static assert

---
+++ src/Utils/Assert.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef __cplusplus
+#if __cplusplus >= 201103L
+#define STATIC_ASSERT(expr) static_assert(expr, "static asssert failed: " #expr)
+#else
+template <bool> class TStaticAssert;
+template <> class TStaticAssert<true>
+{
+};
+
+#define STATIC_ASSERT(expr)                                                                                  \
+    enum                                                                                                     \
+    {                                                                                                        \
+        ASSERT_GLUE(g_assert_fail_, __LINE__) = sizeof(TStaticAssert<!!(expr)>)                              \
+    }
+#endif
+#endif
